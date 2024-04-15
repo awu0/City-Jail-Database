7fd48a5ddb403127953c6e2b95d18c15f71c11bc
Aaron Wu <82164146+awu0@users.noreply.github.com>
2024-04-15 18:14:24 -0400

    the commit before database setup

--- CityJailDatabaseProject/settings.py
+++ CityJailDatabaseProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'CityJailDatabaseProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        # 'USER'
+        # 'PASSWORD',
+        # 'HOST',
+        # 'PORT'
     }
 }
 
