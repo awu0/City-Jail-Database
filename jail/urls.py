7ec588eabaaed4b49a1dd36469bb9564f8257601
awu0 <82164146+awu0@users.noreply.github.com>
2024-04-18 12:29:54 -0400

    did sentence

--- jail/urls.py
+++ jail/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from jail.views import views
-from jail.views import criminal_views, alias_views, crime_views, probation_officer_views
+from jail.views import criminal_views, alias_views, crime_views, probation_officer_views, sentence_views
 
 urlpatterns = [
     # home paths
@@ -37,4 +37,10 @@ urlpatterns = [
          name='probation_officer_add'),
     path('probation_officer/<int:pk>/delete', probation_officer_views.ProbationOfficerDeleteView.as_view(),
          name='probation_officer_delete'),
+
+    # sentence paths
+    path('sentence/', sentence_views.SentenceHomeView.as_view(), name='sentence_home'),
+    path('sentence/<int:pk>/update/', sentence_views.SentenceUpdateView.as_view(), name='sentence_update'),
+    path('sentence/add/', sentence_views.SentenceFormView.as_view(), name='sentence_add'),
+    path('sentence/<int:pk>/delete/', sentence_views.SentenceDeleteView.as_view(), name='sentence_delete'),
 ]
---
+++ jail/views/sentence_views.py
@@ -0,0 +1,42 @@
+from django.views import generic
+
+from jail.models import Sentence
+
+
+class SentenceHomeView(generic.ListView):
+    template_name = 'sentence/index.html'
+    context_object_name = 'sentence_list'
+
+    def get_queryset(self):
+        return Sentence.objects.order_by('criminal')
+
+
+class SentenceUpdateView(generic.UpdateView):
+    # TODO: restrict to certain users
+    model = Sentence
+    template_name = 'sentence/update.html'
+    fields = [
+        'type',
+        'start_date',
+        'end_date',
+        'violations'
+    ]
+
+    success_url = '/sentence/'
+
+
+class SentenceFormView(generic.CreateView):
+    """
+    For adding a new crime
+    """
+    template_name = 'sentence/add.html'
+    model = Sentence
+    fields = '__all__'
+    success_url = '/Sentence/'
+
+
+class SentenceDeleteView(generic.DeleteView):
+    model = Sentence
+
+    template_name = 'sentence/delete.html'
+    success_url = '/sentence/'
